0d3a8e945a7507b96c7190016e503606dcffc282
Syndicates Era <syndicatesera@Syndicatess-MacBook-Air.local>
2019-06-10 18:15:18 +0530

    url changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var io=require("socket.io-client");
 
 function RodoClass(merchantId, secretKey){
     this.merchantId=merchantId;
-    this.socket=io("http://merchant.rodoindia.com:4200");
+    this.socket=io("https://merchantsocket.rodoindia.com");
 
     this.socket.on("connect", ()=>{
         console.log("connected");
